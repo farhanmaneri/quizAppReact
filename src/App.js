f932b92aca602a8f3d1dccfc9f0f6c0c5982b00b
farhanmaneri <farhanmaneri@gmail.com>
2023-08-24 11:34:39 -0700

    changes made

--- src/App.js
+++ src/App.js
@@ -5,66 +5,79 @@ import './App.css'
 // "list-group-item active"
 
 
-const getOptionClass = (qid, op, attempts) => {
+// const getOptionClass = (qid, op, attempts) => {
 
-  let found = attempts.find(attempt => attempt.qid ===qid && attempt.op ===op);
-  if(found){
-  return "list-group-item active"
- }else {
-  return "list-group-item";
-
- }
-
-}
+//   let found = attempts.find(attempt => attempt.qid ===qid && attempt.op ===op);
+//   if(found){
+//   return "btn btn-primary"
+//  }else {
+//   return "btn btn-primary disabled";
+//  }
+// }
 
 export default function App() {
   let [attempts, setAttempts] = useState([]);
   let [currentQuestion, setCurrentQuestion]=useState(0);
-  // const [selectedOptions, setSelectedOptions] = useState({}); // Keep track of selected options
+  const [score, setScore] = useState(0); // Keep track of selected options
+  let [showResult, setShowResult] = useState(false)
 
   const handleOptionSelection = (qid, op) => {
     let attempt = {qid: qid, op: op};    
     setAttempts([...attempts, attempt]); // state update ?
     handleNextQuestion()
+    handleAnswerButtonClick(op)
   }
 
 
-    // const handleAnswerButtonClick = (answerOption) =>{
-  //   console.log(answerOption)
-  //   if(answerOption === questions[currentQuestion].answerIndex){
-  //     console.log('correct')
+    const handleAnswerButtonClick = (op) =>{
+    const correct = op === questions[currentQuestion].answerIndex
+    if(correct){
+      setScore(score +1)
+      console.log('correct')
+      console.log(score)
 
-  //   }else{
-  //     console.log('wrong')
-  //   }
-   
+    }else{
+      console.log('wrong')
+    }
+  }
+  const handleRestButton = ()=>{
+  setScore(0);
+  setCurrentQuestion(0)
+  setShowResult(false)
   
+  }
   const handleNextQuestion = ()=>{
     const nextQuestion = currentQuestion +1;    
     if(nextQuestion<questions.length){
       setCurrentQuestion(nextQuestion);
+      
     }else{
-      alert('Quiz completed')
+      setShowResult(true)
     }
   }
  
   
   return (
     <>
+    {!showResult ?
     <div className="container">
-      <h1>GK Quiz</h1>
+      <div style={{width:'100%',display:'flex', justifyContent: 'center'}}>
+      <h1>Quiz</h1>
+      </div>
       {questions[currentQuestion] && ( 
         <div>
-          <h4>{questions[currentQuestion].statement}</h4>
           <div className="answerSection">
+            <div style={{display:'flex', flexWrap:'wrap',background:'red',borderRadius:'4px',margin:'5px', color:'white', padding:'5px'}}><h5>Question {currentQuestion+1}/{questions.length} <h5>{questions[currentQuestion].statement}</h5></h5>         
+</div>
           <ul className="list-group" >
             {questions[currentQuestion].options.map(op => (
-              <li 
-                key={op}
-                className={getOptionClass(questions[currentQuestion].id, op, attempts)}
-                onClick={() => handleOptionSelection(questions[currentQuestion].id, op)}
-              >
+              <li key={op}><button  
+              className= 'list-group-item'       
+                 // className={getOptionClass(questions[currentQuestion].id, op, attempts)}
+              onClick={() => handleOptionSelection(questions[currentQuestion].id, op)}
+            >
                 {op}
+              </button>
               </li>
             ))}
           </ul></div>
@@ -73,6 +86,14 @@ export default function App() {
       {/* <button onClick={handlePreviousQuestion} disabled={currentQuestion === 0}>Previous</button> */}
       {/* <button onClick={handleNextQuestion} disabled={currentQuestion === questions.length }>Next</button> */}
     </div>
-  </>
+:  (<>
+    <div className="resultDev">
+      <h1> You scored {score} / out of {questions.length}</h1>
+      <div>
+        <button onClick={() =>handleRestButton()}>Try Again</button>
+        </div>
+    </div>
+ </> )  }
+    </>
   );
 }
